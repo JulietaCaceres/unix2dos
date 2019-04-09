8e01ac011889fcc4eb4cc0c3e36f0951fd8f3708
Santiago <santidorgan@gmail.com>
2019-04-09 18:00:13 -0300

    arreglo exception tests

--- exceptionTest.sh
+++ exceptionTest.sh
@@ -1,116 +1,62 @@
-#!/bin/basht
-
-# Test counter.
-COUNT_TEST=0;
-
-# Definition of colors:
-RED="\e[31m";
-GREEN="\e[32m";
-MAGENTA="\e[35m"
-DEFAULT="\e[97m";
-
-function header() {
-  echo -e "$MAGENTA*******************************************************************************************************$DEFAULT";
-  echo -e "        $DEFAULT$1$DEFAULT";
-  echo -e "$MAGENTA*******************************************************************************************************$DEFAULT";
-}
+#!/bin/bash
 
 
-function inputIsNotFoundUnix2dosExceptionTest(){
+
+
+#inputIsNotFoundUnix2dosExceptionTest
+  comm="./unix2dos -i NotFound"
+  echo  ""
+  echo  "*******************************************************************************************************"
+  echo  "                   Test Unix2dos : Input file is not found. "
+  echo  "                   Input File is called NotFound"
+  echo  "                   Expected Result: Can not open input file 'NotFound' "
+  OUTPUT=$($comm 2>&1);
+  echo  "                   Obtained Result: $OUTPUT";
+
+  echo  "*******************************************************************************************************"
+
+  echo ""
+  echo ""
   
-  COUNT_TEST=$(($COUNT_TEST+1));
-  comm="./unix2dos -i ./fileTest/test$COUNT_TEST";
+#fileInputEqualsToFileOutputUnix2dosException
+  comm="./unix2dos -i ./testFiles/test2 -o ./testFiles/test2";
 
-  header  "Test Unix2dos : Input file is not found."
+ 
+  echo  "*******************************************************************************************************"
+  echo  "                   Test Unix2dos : Input and output file ought to be differents. "
+  echo  "                   Input File is called ./testFiles/test2"
+  echo  "                   Expected Result: Input and output ought to be different "
+  OUTPUT=$($comm 2>&1);
+  echo  "                   Obtained Result: $OUTPUT";
 
-  echo -e "$comm\n" 
-  
-  EXPECTED_OUTPUT_INCORRECT_INPUT=("Can not open input file './fileTest/test$COUNT_TEST'")
+  echo  "*******************************************************************************************************"
 
-  OUTPUT=$($comm 2>&1)
+  echo ""
+  echo ""
 
-    if [[ "$EXPECTED_OUTPUT_INCORRECT_INPUT" == "$OUTPUT" ]]; then
+#inputIsNotFoundDos2unixExceptionTest
+  comm="./dos2unix -i NotFound";
 
-      echo -e "Status:$GREEN OK \n $DEFAULT EXCEPTION OUTPUT:\n\t$OUTPUT\n";
-    else
-      echo -e "Status:$RED ERROR \n $DEFAULT PROGRAM OUTPUT:\n\t$OUTPUT  and expected: $EXPECTED_OUTPUT_INCORRECT_INPUT\n";
-      
-    fi
+  echo  "*******************************************************************************************************";
+  echo  "                 Test dos2unix: Input file is not found.";
+  echo  "                   Input File is called NotFound"
+  echo  "                   Expected Result: Can not open input file 'NotFound' "
+  OUTPUT=$($comm 2>&1);
+  echo  "                   Obtained Result: $OUTPUT";
 
-}
+  echo  "*******************************************************************************************************"
 
-function fileInputEqualsToFileOutputUnix2dosException(){
+  echo ""
+  echo ""
 
-  COUNT_TEST=$(($COUNT_TEST+1));
-  comm="./unix2dos -i ./testFiles/test$COUNT_TEST -o ./testFiles/test$COUNT_TEST";
+#fileInputEqualsToFileOutputDos2unixException
+  comm="./dos2unix -i ./testFiles/test2 -o ./testFiles/test2";
 
-  header  "Test Unix2dos: Input and output file ought to be differents."
+  echo  "*******************************************************************************************************"
+  echo  "                 Test dos2unix: Input and output file ought to be differents.";
+  echo  "                   Input File is called ./testFiles/test2"
+  echo  "                   Expected Result: Input and output ought to be different "
+  OUTPUT=$($comm 2>&1);
+  echo  "                   Obtained Result: $OUTPUT";
 
-  echo -e "$comm\n" 
-
-  EXPECTED_OUTPUT_INCORRECT_INPUT=("Input and output ought to be different")
-
-  OUTPUT=$($comm 2>&1)
-
-    if [[ "$EXPECTED_OUTPUT_INCORRECT_INPUT" == "$OUTPUT" ]]; then
-
-      echo -e "Status:$GREEN OK \n $DEFAULT EXCEPTION OUTPUT:\n\t$OUTPUT\n";
-    else
-      echo -e "Status:$RED ERROR \n $DEFAULT PROGRAM OUTPUT:\n\t$OUTPUT  and expected: $EXPECTED_OUTPUT_INCORRECT_INPUT\n";
-
-    fi
-
-}
-
-function inputIsNotFoundDos2unixExceptionTest(){
-
-  COUNT_TEST=$(($COUNT_TEST+1));
-  comm="./dos2unix -i ./fileTest/test$COUNT_TEST";
-
-  header  "Test dos2unix: Input file is not found."
-
-  echo -e "$comm\n" 
-
-  EXPECTED_OUTPUT_INCORRECT_INPUT=("Can not open input file './fileTest/test$COUNT_TEST'")
-
-  OUTPUT=$($comm 2>&1)
-
-    if [[ "$EXPECTED_OUTPUT_INCORRECT_INPUT" == "$OUTPUT" ]]; then
-
-      echo -e "Status:$GREEN OK \n $DEFAULT EXCEPTION OUTPUT:\n\t$OUTPUT\n";
-    else
-      echo -e "Status:$RED ERROR \n $DEFAULT PROGRAM OUTPUT:\n\t$OUTPUT  and expected: $EXPECTED_OUTPUT_INCORRECT_INPUT\n";
-
-    fi
-
-}
-
-function fileInputEqualsToFileOutputDos2unixException(){
-
-  COUNT_TEST=$(($COUNT_TEST+1));
-  comm="./dos2unix -i ./testFiles/test$COUNT_TEST -o ./testFiles/test$COUNT_TEST";
-
-  header  "Test dos2unix: Input and output file ought to be differents."
-
-  echo -e "$comm\n" 
-
-  EXPECTED_OUTPUT_INCORRECT_INPUT=("Input and output ought to be different")
-
-  OUTPUT=$($comm 2>&1)
-
-    if [[ "$EXPECTED_OUTPUT_INCORRECT_INPUT" == "$OUTPUT" ]]; then
-
-      echo -e "Status:$GREEN OK \n $DEFAULT EXCEPTION OUTPUT:\n\t$OUTPUT\n";
-    else
-      echo -e "Status:$RED ERROR \n $DEFAULT PROGRAM OUTPUT:\n\t$OUTPUT  and expected: $EXPECTED_OUTPUT_INCORRECT_INPUT\n";
-
-    fi
-
-}
-
-
-inputIsNotFoundUnix2dosExceptionTest
-fileInputEqualsToFileOutputUnix2dosException
-inputIsNotFoundDos2unixExceptionTest
-fileInputEqualsToFileOutputDos2unixException
-
+  echo  "*******************************************************************************************************"
